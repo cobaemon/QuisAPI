ad9d75516e4ec4e8fffb964355137aaa65256b9f
cobaemon <mgnco@outlook.jp>
2022-04-20 02:52:30 +0900

    Update admin.py
    カスタムユーザの追加

--- quisapi/admin.py
+++ quisapi/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from quisapi.models import Follow, QuizGroup, Quiz
+from quisapi.models import Follow, QuizGroup, Quiz, QuisAPIUser
 
+admin.site.register(QuisAPIUser)
 admin.site.register(QuizGroup)
 admin.site.register(Quiz)
 admin.site.register(Follow)
