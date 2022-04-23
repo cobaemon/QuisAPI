32c4254524da07c1db057747086c052de7437d90
rincham <rincham.dev@gmail.com>
2022-04-23 23:47:41 +0900

    docker-composeの修正

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -5,9 +5,8 @@ set -e
 host="$1"
 shift
 cmd="$@"
-password=$POSTGRES_PASSWORD
 
-until PGPASSWORD="$password" psql -h "$host" -U "quisapi" -c '\l'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "quisapi" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
