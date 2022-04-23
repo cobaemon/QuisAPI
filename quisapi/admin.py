e61e7b24d757345e8aecd7d6a6db7ad00ef2646e
rincham <rincham.dev@gmail.com>
2022-04-23 18:24:28 +0900

    requirements修正

--- quisapi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from quisapi.models import QuizGroup, Quiz, QuisAPIUser, Follower
-
-admin.site.register(QuisAPIUser)
-admin.site.register(QuizGroup)
-admin.site.register(Quiz)
-admin.site.register(Follower)
