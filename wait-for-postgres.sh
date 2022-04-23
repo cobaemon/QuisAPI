6992fd97f7fb934aaedbbc38065ddb3c73fb2f83
rincham <rincham.dev@gmail.com>
2022-04-23 22:52:12 +0900

    ファイル追加

---
+++ wait-for-postgres.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+password=$POSTGRES_PASSWORD
+
+until PGPASSWORD="$password" psql -h "$host" -U "quisapi" -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
